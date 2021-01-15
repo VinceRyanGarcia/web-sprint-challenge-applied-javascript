2f67014bac05cb72c8c4eeab42cdb40171203638
Vince Garcia <vinceryangarcia@gmail.com>
2021-01-15 08:43:07 -0800

    Questions Answered

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,13 @@ const Header = (title, date, temp) => {
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
   //
   //  <div class="header">
+  const header = document.createElement('div')
   //    <span class="date">{ date }</span>
+  const headerDate = document.createElement('span')
   //    <h1>{ title }</h1>
+  const headerTitle = document.createElement('h1')
   //    <span class="temp">{ temp }</span>
+  const headerTemp = document.createElement('span')
   //  </div>
   //
 }
@@ -20,6 +24,10 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+header.appendChild(headerDate)
+header.appendChild(headerTitle)
+header.appendChild(headerTemp)
+
 }
 
 export { Header, headerAppender }
